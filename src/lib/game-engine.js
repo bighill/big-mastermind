779d467877885ac507f1d4010d855a78d8100053
lex <alex@bighill.org>
2020-02-16 01:09:55 -0800

    proc correctColor_WrongPlacement

--- src/lib/game-engine.js
+++ src/lib/game-engine.js
@@ -16,13 +16,22 @@ export const Answer = function(allowRepeatsColors) {
 }
 
 export const Proc = (answer, guess) => {
-  const correctColor_CorrectPlacement = answer.reduce((acc, curr, i) => {
-    return curr.color_i === guess[i].color_i ? acc + 1 : acc
+  const answer__color_i__arr = answer.map(a => a.color_i)
+
+  const correctColor_CorrectPlacement = guess.reduce((acc, curr, i) => {
+    // if color AND placement align, add to `acc`
+    return curr.color_i === answer__color_i__arr[i] ? acc + 1 : acc
   }, 0)
 
-  const correctColor_WrongPlacement = answer.reduce((acc, curr, i) => {
-    return null
-  })
+  const correctColor_WrongPlacement = guess.reduce((acc, curr, i) => {
+    if (answer__color_i__arr.indexOf(curr.color_i) < 0) {
+      // no change if correct color is not present
+      return acc
+    }
+    // color is correct.
+    // if color is misplaced, add to `acc`
+    return curr.color_i !== answer[i].color_i ? acc + 1 : acc
+  }, 0)
 
   return { correctColor_CorrectPlacement, correctColor_WrongPlacement }
 }
