eb12ea9c99a16709d1087ae904967528e7fac3a4
lex <alex@bighill.org>
2020-02-21 23:38:43 -0800

    Test favicon images with manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,25 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Big Mastermind",
+        short_name: "Big Mastermind",
+        start_url: "/",
+        background_color: "tomato",
+        theme_color: "skyblue",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+  ],
 }
